187068a81adfd38e66c7489bfb66835a26614ce1
DESKTOP-RLQEV9L\sufian <abusufian12@gmail.com>
2022-08-27 21:48:00 +0900

    first push

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function add(a: number, b: number){
+  return a + b;
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome to React and Typescript</h2>
+      <p>{add(10, 15)}</p>
     </div>
   );
 }
